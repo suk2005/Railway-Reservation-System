9f6e4ada622fb0aff1049c127d74dce748d04a74
suk2005 <sumitkambli16@gmail.com>
2026-09-17 09:08:10 +0530

    Data Update

---
+++ Loops.py
@@ -0,0 +1,11 @@
+
+
+for i in range (11, 0, -1):
+    print(i)
+
+i=11
+
+i = 10
+while i>=1:
+    print(i)
+    i=i-1
---
+++ while_loop.py
@@ -0,0 +1,13 @@
+num = int(input("Enter Number:"))
+i=1
+while i <= 10:
+    print(num*i)
+    i=i+1
+
+
+
+student = ["a", "b", "c"]
+print(student)
+
+for i in student:
+    print(i)
